079b61b37e4e6b4845f4efd36a4fff78f842ae10
Daniyar <daniyar.nurgaliyev@babel.es>
2020-02-02 13:57:27 +0100

    Added fragment

--- app/src/main/java/com/dd/coroutineroomdatabase/UserApplication.kt
+++ app/src/main/java/com/dd/coroutineroomdatabase/UserApplication.kt
@@ -5,10 +5,12 @@ import android.content.Context
 import com.dd.coroutineroomdatabase.database.UserDAO
 import com.dd.coroutineroomdatabase.database.UserDatabase
 import com.dd.coroutineroomdatabase.database.UserRepository
+import com.dd.coroutineroomdatabase.user.UserViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class UserApplication : Application(), KodeinAware {
@@ -22,6 +24,7 @@ class UserApplication : Application(), KodeinAware {
         bind<UserDAO>() with singleton { instance<UserDatabase>().userDAO }
         bind<UserRepository>() with singleton { UserRepository(instance()) }
 //        bind<UserViewModelFactory>() with provider { UserViewModelFactory(instance()) }
+        bind<UserViewModelFactory>() with provider { UserViewModelFactory(instance()) }
     }
 
 
--- app/src/main/java/com/dd/coroutineroomdatabase/user/MainFragment.kt
+++ app/src/main/java/com/dd/coroutineroomdatabase/user/MainFragment.kt
@@ -6,16 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.dd.coroutineroomdatabase.R
+import com.dd.coroutineroomdatabase.database.User
+import kotlinx.android.synthetic.main.fragment_main.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.support.closestKodein
+import org.kodein.di.generic.instance
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment(), KodeinAware {
+    override val kodein: Kodein by closestKodein()
+
+    private lateinit var userViewModel: UserViewModel
+
+    private val userViewModelFactory: UserViewModelFactory by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,39 +36,42 @@ class MainFragment : Fragment(), KodeinAware {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override val kodein: Kodein by closestKodein()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewModel()
+        showUsersList()
+        init()
+    }
 
+    private fun initViewModel() {
+//        val userDatabase = UserDatabase.getInstance(application)
+//        val dao = userDatabase.userDAO
+//        val repository = UserRepository(dao)
+//        val viewModelFactory = UserViewModelFactory(repository)
+        userViewModel = ViewModelProvider(this, userViewModelFactory).get(UserViewModel::class.java)
+    }
 
-//    private fun initViewModel() {
-////        val userDatabase = UserDatabase.getInstance(application)
-////        val dao = userDatabase.userDAO
-////        val repository = UserRepository(dao)
-////        val viewModelFactory = UserViewModelFactory(repository)
-//
-//        userViewModel = ViewModelProvider(this, userViewModelFactory).get(UserViewModel::class.java)
-//    }
-//
-//    private fun init() {
-//        btn_add.setOnClickListener {
-//            val user = User(0, tv_name.text.toString(), tv_hobby.text.toString())
-//            userViewModel.insert(user)
-//        }
-//
-//        btn_clear.setOnClickListener {
-//            userViewModel.clearAll()
-//        }
-//    }
-//
-//    private fun showUsersList() {
-//        userViewModel.users.observe(this, Observer {
-//            setDataToRecyclerView(it)
-//        })
-//    }
-//
-//    private fun setDataToRecyclerView(userList: List<User>) {
-//        recyclerViewMain.apply {
-//            layoutManager = LinearLayoutManager(this@MainActivity)
-//            adapter = AdapterUser(userList)
-//        }
-//    }
+    private fun init() {
+        btn_add.setOnClickListener {
+            val user = User(0, tv_name.text.toString(), tv_hobby.text.toString())
+            userViewModel.insert(user)
+        }
+
+        btn_clear.setOnClickListener {
+            userViewModel.clearAll()
+        }
+    }
+
+    private fun showUsersList() {
+        userViewModel.users.observe(viewLifecycleOwner, Observer {
+            setDataToRecyclerView(it)
+        })
+    }
+
+    private fun setDataToRecyclerView(userList: List<User>) {
+        recyclerViewMain.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = AdapterUser(userList)
+        }
+    }
 }
